e28125540f7b1fbd41f4ff017f421d4d94e82fa8
Fiordi <fiordi_001@hotmail.com>
2023-11-01 16:36:11 +0100

    fixed download

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using OfficeOpenXml;
+using OfficeOpenXml.Table;
+
+using System.IO;
 
 namespace checkgrades.Pages;
 
@@ -15,5 +20,27 @@ public class IndexModel : PageModel
     public void OnGet()
     {
 
+    }
+        public IActionResult OnGetDownloadFile()
+    {
+        var students = new[]
+        {
+            new { StudentId = 1, Name = "John Doe", Grade = "A" },
+            new { StudentId = 2, Name = "Jane Doe", Grade = "B" }
+        };
+
+        using (var package = new ExcelPackage())
+        {
+            var worksheet = package.Workbook.Worksheets.Add("StudentData");
+
+            worksheet.Cells.LoadFromCollection(students, true, TableStyles.None);
+
+            var stream = new MemoryStream(package.GetAsByteArray());
+
+            return new FileStreamResult(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+            {
+                FileDownloadName = "student_data.xlsx"
+            };
+        }
     }
 }
